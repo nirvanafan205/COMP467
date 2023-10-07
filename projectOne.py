db5e8761c919d1c43d3f8a13e1becb011cf66c37
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-10-06 22:01:06 -0700

    outputed into csv file

--- projectOne.py
+++ projectOne.py
@@ -1,8 +1,9 @@
+import csv
+
 # Initialize names of what you need to save
 producer = ""
 operator = ""
 job = ""
-location = []
 notes = ""
 
 # Open the file in read mode
@@ -17,24 +18,42 @@ for line in data:
     # Save names and such
     if line.startswith("Producer:"):
         producer = line[len("Producer:"):].strip()
+
     elif line.startswith("Operator:"):
         operator = line[len("Operator:"):].strip()
+
     elif line.startswith("Job:"):
         job = line[len("Job:"):].strip()
-    elif line.startswith("/"):
-        location.append(line)
-    elif line.startswith("Notes:"):
-        capture_notes = True  # Start capturing notes
-        notes = []  # Initialize notes as a list
-    elif capture_notes:
-        notes.append(line)  # Capture all lines after "Notes:" until the end
 
-# Printing
-print("Producer:", producer)
-print("Operator:", operator)
-print("Job:", job)
-print("Location:", location)
-if notes:
-    print("Notes:")
-    for note_line in notes:
-        print(note_line)
+    elif line.startswith("Notes:") or line.startswith("Notes :"):
+        # Start capturing notes
+        capture_notes = True
+        notes = []  # Initialize notes as a list
+
+    elif capture_notes:
+        # Capture all lines after "Notes:" (including leading spaces)
+        notes.append(line)
+
+# Join the lines of the "Notes" section into a single string
+notes = ' '.join(notes).strip()
+
+# Create a dictionary to store data
+data_dict = {
+        "Producer": producer,
+        "Operator": operator,
+        "Job": job,
+        "Notes": notes,
+        }
+
+# Export data to CSV file
+with open('output.csv', 'w', newline='') as csv_file:
+    fieldnames = ["Producer", "Operator", "Job", "Notes"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    # Write header
+    writer.writeheader()
+
+    # Write data
+    writer.writerow(data_dict)
+
+print("Data exported to 'output.csv'")
