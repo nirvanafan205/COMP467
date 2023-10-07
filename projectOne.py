ed1245e710a42d47429dd666106c692c9ce00cc1
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2023-10-06 20:47:51 -0700

    printed notes

--- projectOne.py
+++ projectOne.py
@@ -1,37 +1,40 @@
-# initialized names of what I need to save
+# Initialize names of what you need to save
 producer = ""
 operator = ""
 job = ""
 location = []
 notes = ""
 
-# Open the files in read mode
+# Open the file in read mode
 with open('Xytech.txt', 'r') as file:
-    data= file.read()
+    data = file.readlines()
 
-# parsing data
+# Parsing data line by line
+capture_notes = False  # Flag to capture notes
 for line in data:
     line = line.strip()
 
-        # save names and such 
+    # Save names and such
     if line.startswith("Producer:"):
         producer = line[len("Producer:"):].strip()
-
     elif line.startswith("Operator:"):
         operator = line[len("Operator:"):].strip()
-
     elif line.startswith("Job:"):
         job = line[len("Job:"):].strip()
-
     elif line.startswith("/"):
         location.append(line)
-
     elif line.startswith("Notes:"):
-        notes = line[len("Notes:"):].strip()
+        capture_notes = True  # Start capturing notes
+        notes = []  # Initialize notes as a list
+    elif capture_notes:
+        notes.append(line)  # Capture all lines after "Notes:" until the end
 
-# printing 
+# Printing
 print("Producer:", producer)
 print("Operator:", operator)
 print("Job:", job)
 print("Location:", location)
-print("Notes:", notes)
+if notes:
+    print("Notes:")
+    for note_line in notes:
+        print(note_line)
